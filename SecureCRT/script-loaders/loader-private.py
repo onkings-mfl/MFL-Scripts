3d04c422e09d8cb3b59f5804d5e3d900d154e941
onkings-mfl <lenisd29@gmail.com>
2025-09-10 15:48:11 -0400

    v5

--- SecureCRT/script-loaders/loader-private.py
+++ SecureCRT/script-loaders/loader-private.py
@@ -3,6 +3,6 @@ from urllib.request import Request
 
 url = "https://raw.githubusercontent.com/onkings-mfl/MFL-Scripts/refs/heads/main/SecureCRT/script-logins/login.py"
 req = Request(url)
-req.add_header("Authorization", "token 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")
+req.add_header("Authorization", "token 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")
 with urllib.request.urlopen(req) as response:
     code = response.read().decode('utf-8')
